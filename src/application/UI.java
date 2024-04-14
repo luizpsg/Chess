646375f32bc8ff201646a4fdd3cd00dcd379c8f1
luizpsg <999785@sga.pucminas.br>
2024-04-14 20:07:31 -0300

    Refactor UI class to fix ANSI color code issue in printPiece method

--- src/application/UI.java
+++ src/application/UI.java
@@ -87,7 +87,7 @@ public class UI {
       if (piece.getColor() == Color.WHITE) {
         System.out.print(ANSI_WHITE + piece + ANSI_RESET);
       } else {
-        System.out.println(ANSI_YELLOW + piece + ANSI_RESET);
+        System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
       }
     }
     System.out.print(" ");
