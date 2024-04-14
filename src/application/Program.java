27adb6c16fd7565ff2158d2ae04363a2556a8656
luizpsg <999785@sga.pucminas.br>
2024-04-14 20:03:07 -0300

    Refactor UI class to use printMatch method in Program.java

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,7 +17,7 @@ public class Program {
       while (true) {
         try {
           UI.clearScreen(); 
-          UI.printBoard(chessMatch.getPieces());
+          UI.printMatch(chessMatch);
           System.out.println();
           System.out.print("Source: ");
           ChessPosition source = UI.readChessPosition(sc);
@@ -25,7 +25,6 @@ public class Program {
           boolean[][] possibleMoves = chessMatch.possibleMoves(source);
           UI.clearScreen();
           UI.printBoard(chessMatch.getPieces(), possibleMoves);
-          
           System.out.println();
           System.out.print("Target: ");
           ChessPosition target = UI.readChessPosition(sc);
